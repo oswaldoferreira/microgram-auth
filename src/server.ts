e8296288eb34df30ba30232639d7494afb4de70a
Oswaldo Ferreira <oswluizf@gmail.com>
2020-10-28 19:02:20 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_USER_MODELS} from './controllers/v0/model.index';
+
+
+(async () => {
+  await sequelize.addModels(V0_USER_MODELS);
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 5000;
+
+  app.use(bodyParser.json());
+
+  app.use(cors({
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: "http://a12e954cc71d24037bc1361adb33a804-1927388553.sa-east-1.elb.amazonaws.com",
+  }));
+
+  app.use('/api/v0/', IndexRouter);
+
+  // Root URI call
+  app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
+  } );
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ${config.url}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
